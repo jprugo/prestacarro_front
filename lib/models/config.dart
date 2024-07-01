56476d62aa217e56fceab0371b9f4d6665e82669
JuanPablo <ruedag.jp@gmail.com>
2024-07-01 13:07:46 -0500

    Fin integracion prestacarro viejo

--- lib/models/config.dart
+++ lib/models/config.dart
@@ -1,38 +1,46 @@
 class Config {
+  late String title;
   late String backendBaseUrl;
   late String cameraBaseUrl;
   late List<String> nodes;
   late String logoPath;
   late String logo2;
+  late String avatar;
   late bool selectionMenuEnabled;
 
   Config({
+    required this.title,
     required this.backendBaseUrl,
     required this.cameraBaseUrl,
     required this.nodes,
     required this.logoPath,
     required this.logo2,
+    required this.avatar,
     required this.selectionMenuEnabled
   });
 
   factory Config.fromJson(Map<String, dynamic> json) {
     return Config(
+      title: json['title'] as String,
       backendBaseUrl: json['backendBaseUrl'] as String,
       cameraBaseUrl: json['cameraBaseUrl'] as String,
       nodes: (json['nodes'] as List<dynamic>).cast<String>(),
       logoPath: json['logoPath'] as String,
       logo2: json['logo2'] as String,
+      avatar: json['avatar'] as String,
       selectionMenuEnabled: json['selectionMenuEnabled'] as bool,
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
+      'title': backendBaseUrl,
       'backendBaseUrl': backendBaseUrl,
       'cameraBaseUrl': cameraBaseUrl,
       'nodes': nodes,
       'logoPath': logoPath,
       'logo2': logo2,
+      'avatar': avatar,
       'selectionMenuEnabled': selectionMenuEnabled
     };
   }
